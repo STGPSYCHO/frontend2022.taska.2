837962907989694e1fc45fa2a1111ba1431326a9
Andrey Bubenko <a.bubenko@mail.ru>
2022-11-20 15:04:23 +0700

    Исправление комментариев по PR

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,35 @@
-const filmButton = document.querySelector('#film-name-button');
-function handlerProperty(e){
-  console.log(e)
-  const films = ['Алиса в стране чудес', 'Мстители','Город мертвых', 'Призрачный гонщик']
+let grid_display_article = true;
 
+/*Блок фильтра поиска фильмов*/ 
 
-  // Посмотреть innerHTML. и обнулить контейнер
-  const filmsList = document.querySelector('.usefull-info-left__list');
-  filmsList.innerHTML = ''
-  // while (filmsList.firstChild) {                       Было / Стало
-  //   filmsList.removeChild(filmsList.firstChild);
-  // }
-  
-  
-  let input = document.getElementById('film-name-text').value ;
-  
-  
-  const filterFilmsList = films.filter( (film) => film.includes(input));
-  
-  const contentString =
-  filterFilmsList.map((item,index) => `
+const filmButton = document.querySelector("#film-name-button");
+const films = [
+  "Алиса в стране чудес",
+  "Мстители",
+  "Город мертвых",
+  "Призрачный гонщик",
+];
+const filmsList = document.querySelector(".usefull-info-left__list");
+function handlerProperty() {
+  let input = document.getElementById("film-name-text").value;
+  filmsList.innerHTML = "";
+  const filterFilmsList = films.filter((film) => film.includes(input));
+  const contentString = filterFilmsList
+    .map(
+      (item, index) => `
       <li class="usefull-info-left__item">${item}</li>
-    `).join('') 
-  filmsList.insertAdjacentHTML('beforeend', contentString);
+    `
+    )
+    .join("");
+  filmsList.innerHTML = contentString;
 }
-if(filmButton){
-  filmButton.addEventListener('click', handlerProperty )  
+if (filmButton) {
+  filmButton.addEventListener("click", handlerProperty);
 }
-handlerProperty('click');
+handlerProperty();
+
+/*Переключение отображения с листа на грид*/ 
+
 const icon__list_button = document.getElementById('icon__list-button');
 const icon__grid_button = document.getElementById('icon__grid-button');
 
@@ -34,9 +37,10 @@ const article_ = document.querySelector('.article');
 
 
 if(icon__list_button){
-  // console.log('zashli v function')
   icon__list_button.addEventListener('click', (e) => {
-    console.log(e)
+
+    grid_display_article = false;
+
     let article_contents = document.querySelectorAll('.article-content');
     let article__contents = document.querySelectorAll('.article__content');
     article_.classList.add('article--list');
@@ -55,7 +59,9 @@ if(icon__list_button){
 if(icon__grid_button){
 
   icon__grid_button.addEventListener('click',(e) => {
-    console.log(e)
+    
+    grid_display_article = true;
+
     let article_contents = document.querySelectorAll('.article-content');
     let article__contents = document.querySelectorAll('.article__content');
     
@@ -74,27 +80,28 @@ if(icon__grid_button){
   });
 }
 
+/*Открытие формы добавления*/
 const article_add__button = document.getElementById('article-add__button'); 
 const article_add__button_cancel = document.getElementById('article-add__button-cancel');
 const article_add__button_create = document.getElementById('article-add__button-create');
 const form = document.querySelector('.form');
 const faq_ = document.querySelector('.faq');
-// form.submit(function (e) {
-//   return false
-// });
 
-
+ 
 if(article_add__button){
-  //console.log('zashliv v dobavleniye')
+
   article_add__button.addEventListener('click', (e) => {
-    //console.log(form);
+
     if(form){
-      faq_.style.display='none';
+
       article_add__button.style.display = 'none';
-      form.style.display = 'flex';
+      form.style.display = 'flex';      
     }
   });
 }
+
+/*Добавление артикла*/ 
+
 if(article_add__button_create){
   form.addEventListener('submit',(e)=>{
     let input_name = document.getElementById('article-text-name').value ;
@@ -102,51 +109,76 @@ if(article_add__button_create){
     let input_text = document.getElementById('article-text-description').value ;
     let now = new Date();
     const articles = document.querySelector('.article');
+    let contentString ="";
+    let get_day = now.toLocaleString('en', {month:'long'}) + " " + now.getDate()+ ", " + now.getFullYear();
 
-    const contentString = 
-    `<article class="article-content">
-    <img class="article__img" src="${input_url}" alt="">
-    <div class="article__content">
-        <div class="article__wrapper">
-            <h3 class="article__title">${input_name}</h3>
-            <p class="article__text">${input_text}</p>    
-        </div>
-        <footer>
-            <p class="article__date">${now.toLocaleString('en', {month:'long'})} ${now.getDate()}, ${now.getFullYear()} · 4 min read</p>
-        </footer>
-    </div>
-    </article>`
+    if (grid_display_article){
+      contentString = 
+      `<article class="article-content">
+      <img class="article__img" src="${input_url}" alt="">
+      <div class="article__content">
+          <div class="article__wrapper">
+              <h3 class="article__title">${input_name}</h3>
+              <p class="article__text">${input_text}</p>    
+          </div>
+          <footer>
+              <p class="article__date">${get_day} · 4 min read</p>
+          </footer>
+      </div>
+      </article>`
+    }
+    else{
+      contentString = 
+      `<article class="article-content article-content--list">
+      <img class="article__img" src="${input_url}" alt="">
+      <div class="article__content article__content--list">
+          <div class="article__wrapper">
+              <h3 class="article__title">${input_name}</h3>
+              <p class="article__text">${input_text}</p>    
+          </div>
+          <footer>
+              <p class="article__date">${get_day} · 4 min read</p>
+          </footer>
+      </div>
+      </article>`
+    }
     articles.insertAdjacentHTML('beforeend', contentString);
 
     if(form){
-      faq_.style.display='block';
       article_add__button.style.display = 'block';
       form.style.display = 'none';
+      e.target.reset();
+      
       };
   });
 }
+
+/*Отмена добавления артикла*/ 
+
 if(article_add__button_cancel){
   article_add__button_cancel.addEventListener('click',(e)=>{
     if(form){
-      faq_.style.display='block';
       article_add__button.style.display = 'block';
       form.style.display = 'none';
     };
   });
 }
 
-const faq = document.getElementsByClassName("faq-page__question");
+/*Скрытие и раскрытие блока faq*/ 
+
+const faq = document.querySelectorAll(".faq-page__question");
 let i;
-for (i = 0; i < faq.length; i++) {
-    faq[i].addEventListener("click", function () {
 
-        this.classList.toggle("active");
+faq.forEach((faq_question) => {
+  faq_question.addEventListener("click", function () {
 
-        let body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
-        } else {
-            body.style.display = "block";
-        }
-    });
-}
+    this.classList.toggle("active");
+
+    let body = this.nextElementSibling;
+    if (body.style.display === "block") {
+        body.style.display = "none";
+    } else {
+        body.style.display = "block";
+    }
+});
+});
